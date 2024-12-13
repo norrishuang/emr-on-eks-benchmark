06f74a938e172cfa135e670b096940c050cb5fb3
norrishuang <12380647@qq.com>
2024-12-13 14:56:39 +0800

    add s3table

--- spark-sql-perf/src/main/scala/com/databricks/spark/sql/perf/tpcds/TPCDS_Iceberg_General.scala
+++ spark-sql-perf/src/main/scala/com/databricks/spark/sql/perf/tpcds/TPCDS_Iceberg_General.scala
@@ -61,7 +61,7 @@ class TPCDS_Iceberg_General(@transient sqlContext: SQLContext,catalog: String, d
       println(s"Query: ${q.name}")
       try {
         println(s"Query SQL: ${q.sqlText}")
-        var df = sqlContext.sql("USE " + database)
+        var dfdb = sqlContext.sql("USE " + database)
         val df = sqlContext.sql(q.sqlText.get)
         if (showPlan) {
           df.explain()
